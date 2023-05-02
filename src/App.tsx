153fc4849ed84292de706086bb0b254172de159b
Mary190183 <pma190183@yandex.ru>
2023-05-02 23:37:14 +0300

    feat: pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import NotFound from './pages/NotFound';
 import Layout from './components/Layout';
 
@@ -10,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="sigh-up" element={<Home />} />
-          <Route path="sigh-in" element={<NotFound />} />
+          <Route path="sigh-in" element={<Login />} />
+          <Route path="sigh-up" element={<Register />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+function Login() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen text-white">
+      <h1 className="text-3xl font-bold underline">Login</h1>
+      <h2 className="text-2xl font-bold mt-4">We start</h2>
+    </div>
+  );
+}
+export default Login;
