d0ab97703ba56c5d2a8147395ba7d119e269cd7a
a.rubashka <a.rubashka@godeltech.com>
2023-05-03 18:17:03 +0200

    feat: integrate firebase into app

---
+++ src/firebase.ts
@@ -0,0 +1,87 @@
+import { initializeApp } from 'firebase/app';
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+  DocumentData,
+  CollectionReference,
+} from 'firebase/firestore';
+
+const app = initializeApp({
+  apiKey: 'AIzaSyDEZA7LtyhlzYL4Fyvefks2G_HTB9ail5s',
+  authDomain: 'graphiql-app-3bb99.firebaseapp.com',
+  projectId: 'graphiql-app-3bb99',
+  storageBucket: 'graphiql-app-3bb99.appspot.com',
+  messagingSenderId: '1050577830539',
+  appId: '1:1050577830539:web:e8d392b38734630658ca8e',
+});
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const { user } = res;
+    const q = query(collection(db, 'users'), where('uid', '==', user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, 'users'), {
+        authProvider: 'google',
+        name: user.displayName,
+        email: user.email,
+        uid: user.uid,
+      } as CollectionReference<DocumentData>);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const { user } = res;
+    await addDoc(collection(db, 'users'), {
+      uid: user.uid,
+      name,
+      authProvider: 'local',
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  logout,
+};
