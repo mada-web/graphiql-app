025fdaf230df120e489c8730b18de25e6a084334
dairinka <interest4me@gmail.com>
2023-05-03 20:27:10 +0300

    feat: add content to 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,29 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function NotFound() {
+export const NotFound: FC = (): JSX.Element => {
+  const navigate = useNavigate();
+  const routeChange = () => {
+    navigate('/');
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen">
-      <h1 className="text-3xl font-bold underline">404</h1>
-      <h2 className="text-2xl font-bold mt-4">Page not found</h2>
+    <div className="mt-20 relative max-w-screen-xl h-[500px]  text-white bg-opacity-25 bg-center bg-no-repeat bg-[url('src/assets/img/bg_404.png')] bg-content">
+      <div className="absolute top-[40px] right-20 text-center">
+        <h1 className="text-7xl font-bold font-Impact tracking-wider">404</h1>
+        <h2 className="text-2xl font-bold mt-4 font-sans">Page not found</h2>
+      </div>
+      <div className="absolute bottom-10 grid grid-cols-3 grid-rows-1 w-4/5 mx-auto inset-x-0 items-center">
+        <span className="justify-self-center">
+          Ups, something went wrong...{' '}
+        </span>
+        <button
+          className="bg-green px-16 py-3 rounded-md justify-self-center"
+          onClick={routeChange}
+        >
+          go to home
+        </button>
+      </div>
     </div>
   );
-}
-export default NotFound;
+};
