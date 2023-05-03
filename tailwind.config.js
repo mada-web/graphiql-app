950ae8a5b6797abd96e17eb1bb78c64b0d799b20
dairinka <interest4me@gmail.com>
2023-05-03 20:26:26 +0300

    feat: add style to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': [ 'Inria Sans, sans-serif'],
+        'Impact': [ 'Impact', 'Arial Black'],
+      },
+      colors: {
+        'green': '#7EBC59',
+        'white': '#ffffff',
+        'gray': '#D9D9D9',
+        'dark-blue': '#0E0E27',
+        'middle-gray': '#A4A4A4'        
+      }
+    },
   },
   plugins: [],
 };
