9cdb7f7afe8a78de0b59c8d9a38b7d39ce9464f9
dairinka <interest4me@gmail.com>
2023-05-04 10:38:32 +0300

    fix:  change picture location

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -8,7 +8,7 @@ const NotFound = (): JSX.Element => {
 
   return (
     <div className="mt-20 w-screen">
-      <div className="max-w-7xl grid grid-cols-3 grid-rows-[200px_auto_140px] h-[500px] mx-auto inset-x-0 text-white bg-center bg-no-repeat  bg-[url('src/assets/img/bg_404.png')] bg-content rounded-[20px]">
+      <div className="max-w-7xl grid grid-cols-3 grid-rows-[200px_auto_140px] h-[500px] mx-auto inset-x-0 text-white bg-center bg-no-repeat  bg-[url('./bg_404.png')] bg-content rounded-[20px]">
         <div className="text-center self-center row-start-2 col-start-1 col-span-3 sm:col-start-3 sm:row-start-1">
           <h1 className="text-7xl font-bold font-Impact tracking-wider">404</h1>
           <h2 className="text-2xl font-bold mt-4 font-sans">Page not found</h2>
