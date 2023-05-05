99d2c08519f5eca1fcceaafbc74061ef17cf7407
a.rubashka <a.rubashka@godeltech.com>
2023-05-05 12:32:54 +0200

    feat: add new husky script

--- lint-staged.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  '*.{ts,tsx}': ['npm run lint', () => 'tsc-files --noEmit'],
-  '*.{ts,tsx,json,css}': ['prettier --write'],
-};
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,7 @@
 const Register = () => {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen text-white">
+
       <h1 className="text-3xl font-bold underline">Register</h1>
       <h2 className="text-2xl font-bold mt-4">We start</h2>
     </div>
