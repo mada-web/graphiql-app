46e854bf93da1ebd7acdd29b9bc14e48ae466124
Mary190183 <pma190183@yandex.ru>
2023-05-05 20:32:46 +0300

    fix: fix @

--- src/components/Form/FormEmail.tsx
+++ src/components/Form/FormEmail.tsx
@@ -31,7 +31,7 @@ const FormEmail: FC<InputEmailProps> = (props) => {
             required: 'Enter a email',
             pattern: {
               value:
-                /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*(?:\@\.)*(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/i,
+                /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*(?:[@])(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/i,
               message: 'Invalid e-mail',
             },
           })}
