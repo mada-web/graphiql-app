5402449266912c53be8daa11f5d1bb391d784e01
Mary190183 <pma190183@yandex.ru>
2023-05-07 17:27:59 +0300

    fix: background

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,6 +1,6 @@
 export const Background = () => {
   return (
-    <div className="md:h-screen h-full sm:flex hidden w-screen flex justify-between items-stretch overflow-hidden z-0">
+    <div className="h-full sm:flex hidden w-screen flex justify-between items-stretch overflow-hidden z-0">
       <div className="relative w-[539px] h-[463px] bg-[url('./element_2.png')] bg-auto -mb-[33px] -ml-[273px] place-self-end" />
       <div className="relative w-[539px] h-[463px] bg-[url('./element_1.png')] bg-auto -mt-[33px] -mr-[232px] bg-no-repeat place-self-start" />
     </div>
