18a46bdf42ff5de0f58a2ccdf8296734bdb17b38
a.rubashka <a.rubashka@godeltech.com>
2023-05-09 10:14:12 +0200

    feat: add locale and logout to burger menu

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -1,13 +1,21 @@
-import { FC } from 'react';
+import { ChangeEvent, FC } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { FormattedMessage } from 'react-intl';
 
 import Close from '../assets/svg/close.svg';
+import { auth, logout } from '../firebase';
+import { LOCALES } from '../lang/locales';
 
 interface ModalProps {
   onClose: () => void;
+  locale: string;
+  handleLocale: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Burger: FC<ModalProps> = ({ onClose }): JSX.Element => {
+export const Burger: FC<ModalProps> = ({ onClose, handleLocale, locale }): JSX.Element => {
+  const [user] = useAuthState(auth);
+
   return (
     <div
       data-testid="modal"
@@ -27,21 +35,53 @@ export const Burger: FC<ModalProps> = ({ onClose }): JSX.Element => {
         >
           <Close />
         </button>
-        <NavLink
-          className="p-3 font-semibold leading-7 transition-all hover:border-b-2"
-          to="sign-in"
-        >
-          Sign in
-        </NavLink>
-        <NavLink
-          className="p-3 font-semibold leading-7 transition-all hover:border-b-2"
-          to="sign-up"
-        >
-          Sign up
-        </NavLink>
-        <NavLink className="p-3 font-semibold leading-7 transition-all hover:border-b-2" to="/">
-          LogOut<span aria-hidden="true">&rarr;</span>
-        </NavLink>
+        {!user && (
+          <>
+            <NavLink
+              className="p-3 font-semibold leading-7 transition-all hover:border-b-2"
+              to="sign-in"
+            >
+              <FormattedMessage id="SIGN_IN" />
+            </NavLink>
+            <NavLink
+              className="p-3 font-semibold leading-7 transition-all hover:border-b-2"
+              to="sign-up"
+            >
+              <FormattedMessage id="SIGN_UP" />
+            </NavLink>
+          </>
+        )}
+        {user && (
+          <>
+            <NavLink
+              className="p-3 font-semibold leading-7 transition-all hover:border-b-2"
+              to="/main"
+            >
+              <FormattedMessage id="MAIN" />
+            </NavLink>
+            <NavLink
+              className="p-3 font-semibold leading-7 transition-all hover:border-b-2"
+              to="/"
+              onClick={logout}
+            >
+              <FormattedMessage id="LOG_OUT" />
+            </NavLink>
+          </>
+        )}
+        <div className="flex px-3 py-2.5">
+          <span className="mr-3 font-semibold ">Ru</span>
+          <label htmlFor="lang" className="relative h-5 w-10 cursor-pointer rounded-full bg-black">
+            <input
+              type="checkbox"
+              id="lang"
+              className="peer sr-only"
+              checked={locale !== LOCALES.RUSSIAN}
+              onChange={(e) => handleLocale(e)}
+            />
+            <span className="peer absolute left-0.5 top-0.5 h-4/5 w-2/5 rounded-full bg-green transition-all duration-200 peer-checked:left-5" />
+          </label>
+          <span className="ml-3 font-semibold">En</span>
+        </div>
       </li>
     </div>
   );
