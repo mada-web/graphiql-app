21db44e758b987bb1ea0e65fbc64caad877c0ad7
dairinka <interest4me@gmail.com>
2023-05-10 22:38:50 +0300

    feat: add new control button

--- src/components/main/ControlBtns.tsx
+++ src/components/main/ControlBtns.tsx
@@ -2,24 +2,39 @@ import { FC } from 'react';
 
 import useAppContext from '../../hooks/useAppContext';
 
-import Play from '../../assets/svg/btn_play.svg';
+import BtnPlay from '../../assets/svg/btn_play.svg';
+import BtnSchema from '../../assets/svg/btn_schema.svg';
+import BtnQuery from '../../assets/svg/btn_query_params.svg';
 
 export interface IControlBtns {
   changeList: (str: string) => void;
 }
 const ControlBtns: FC = (): JSX.Element => {
-  const { queryBody, queryParams } = useAppContext();
+  const { queryBody, queryParams, setIsShowSchema, setIsQueryParams } = useAppContext();
 
   const executeQuery = () => {
     console.log(queryBody, queryParams);
   };
 
+  const showSchema = () => {
+    setIsShowSchema((prev) => !prev);
+  };
+
+  const showQueryParams = () => {
+    setIsQueryParams((prev) => !prev);
+  };
+
   return (
-    <aside className="col-start-2 row-span-2 row-start-1 h-[100%] border-r-0 border-gray sm:border-r-[1px]">
+    <aside className="col-start-2 row-span-2 row-start-1 ml-2 flex h-[100%] flex-col items-start gap-2 border-r-0 border-gray pr-2 sm:items-center sm:border-r-[1px]">
       <button title="Execute query" className="mr-2 h-[30px] w-[30px]" onClick={executeQuery}>
-        <Play />
+        <BtnPlay />
       </button>
-      {/* place for control buttons */}
+      <button title="Show schema" className="mb-3 mr-2 h-[30px] w-[30px]" onClick={showQueryParams}>
+        <BtnQuery />
+      </button>
+      <button title="Show schema" className="mr-2 h-[30px] w-[30px]" onClick={showSchema}>
+        <BtnSchema />
+      </button>
     </aside>
   );
 };
