54cf6d23f2111e14e807f59cd42646e4efc4214e
dairinka <interest4me@gmail.com>
2023-05-11 00:13:07 +0300

    feat: add style to response block

--- src/components/main/ResponseBlock.tsx
+++ src/components/main/ResponseBlock.tsx
@@ -1,5 +1,18 @@
+import useAppContext from '../../hooks/useAppContext';
+
 const ResponseBlock = () => {
-  return <div className="w-1/2 transition-all">{/* place for response */}</div>;
+  const { isShowResult, queryBody, queryParams } = useAppContext();
+
+  return (
+    <section className="min-h-screen w-full grow border-t-8 border-gray bg-dark-blue px-8 pt-16 text-sm text-gray transition-all sm:mt-[120px] sm:min-h-0 sm:w-1/2 sm:border-t-0 sm:px-8 sm:pt-0">
+      {isShowResult && (
+        <div>
+          {queryBody}
+          {queryParams}
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default ResponseBlock;
