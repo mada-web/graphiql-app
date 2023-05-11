8dc338cc95be240b292d00e7df619313715daa07
dairinka <interest4me@gmail.com>
2023-05-11 14:04:16 +0300

    feat: add hover buttons

--- src/components/main/ControlBtns.tsx
+++ src/components/main/ControlBtns.tsx
@@ -3,8 +3,8 @@ import { FC } from 'react';
 import useAppContext from '../../hooks/useAppContext';
 
 import BtnPlay from '../../assets/svg/btn_play.svg';
-import BtnSchema from '../../assets/svg/btn_schema.svg';
 import BtnQuery from '../../assets/svg/btn_query_params.svg';
+import BtnSchema from '../../assets/svg/btn_schema.svg';
 
 export interface IControlBtns {
   changeList: (str: string) => void;
@@ -26,13 +26,25 @@ const ControlBtns: FC = (): JSX.Element => {
 
   return (
     <aside className="col-start-2 row-span-2 row-start-1 ml-2 flex h-[100%] flex-col items-start gap-2 border-r-0 border-gray pr-2 sm:items-center sm:border-r-[1px]">
-      <button title="Execute query" className="mr-2 h-[30px] w-[30px]" onClick={executeQuery}>
+      <button
+        title="Execute query"
+        className="mr-2 h-[30px] w-[30px] transition-all hover:brightness-150"
+        onClick={executeQuery}
+      >
         <BtnPlay />
       </button>
-      <button title="Show schema" className="mb-3 mr-2 h-[30px] w-[30px]" onClick={showQueryParams}>
+      <button
+        title="Query params"
+        className="mb-3 mr-2 h-[30px] w-[30px] transition-all hover:brightness-150"
+        onClick={showQueryParams}
+      >
         <BtnQuery />
       </button>
-      <button title="Show schema" className="mr-2 h-[30px] w-[30px]" onClick={showSchema}>
+      <button
+        title="Show schema"
+        className="mr-2 h-[30px] w-[30px] transition-all hover:brightness-150"
+        onClick={showSchema}
+      >
         <BtnSchema />
       </button>
     </aside>
