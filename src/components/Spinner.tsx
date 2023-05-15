1c414ff33bb24178bcc1385b6ff6f8f888d40199
Mary190183 <pma190183@yandex.ru>
2023-05-15 21:10:36 +0300

    fix: suspense? schema position spinner?

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,26 +2,26 @@ import { FC } from 'react';
 
 export const Spinner: FC = (): JSX.Element => {
   return (
-    <div className="absolute flex h-screen w-screen flex-col items-center justify-center ">
-      <svg
-        className="h-[3.75rem] animate-spin"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
-        <g id="SVGRepo_iconCarrier">
-          <path
-            opacity="0.2"
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-            fill="#7EBC59"
-          />
-          <path d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z" fill="#7EBC59" />
-        </g>
-      </svg>
-    </div>
+    // <div className="absolute flex h-screen w-screen flex-col items-center justify-center ">
+    <svg
+      className="h-[3.75rem] animate-spin"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+      <g id="SVGRepo_iconCarrier">
+        <path
+          opacity="0.2"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+          fill="#7EBC59"
+        />
+        <path d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z" fill="#7EBC59" />
+      </g>
+    </svg>
+    // </div>
   );
 };
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,18 +5,17 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebase';
 import { Spinner } from '../components/Spinner';
 
-import EditorBlock from '../components/main/EditorBlock';
+const EditorBlock = lazy(() => import('../components/main/EditorBlock'));
 import QueryBlock from '../components/main/QueryBlock';
-import ResponseBlock from '../components/main/ResponseBlock';
+const ResponseBlock = lazy(() => import('../components/main/ResponseBlock'));
 const SchemaBlock = lazy(() => import('../components/main/SchemaBlock'));
 import ControlButtons from '../components/main/ControlButtons';
-
 const Main: FC = (): JSX.Element => {
   const [user] = useAuthState(auth);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!user) navigate('/');
+    if (!user) navigate('/main');
   }, [navigate, user]);
 
   return (
@@ -26,13 +25,29 @@ const Main: FC = (): JSX.Element => {
       </div>
       <div className="relative flex flex-col sm:flex-row">
         <div className="relative grid h-screen grid-cols-[80%_20%] grid-rows-[auto_auto] pt-[120px] transition-all sm:w-1/2 sm:grid-cols-[85%_15%] md:grid-cols-[90%_10%]">
-          <EditorBlock />
+          <Suspense
+            fallback={
+              <section className="relative z-0 flex max-h-max min-h-[60%] items-start justify-center pb-2 pl-1 sm:mr-2 sm:h-[calc(100%-1rem)] sm:pl-10">
+                <Spinner />
+              </section>
+            }
+          >
+            <EditorBlock />
+          </Suspense>
           <ControlButtons />
           <div className="relative z-10 col-span-2 col-start-1 row-start-2 flex w-full flex-col self-end">
             <QueryBlock />
           </div>
         </div>
-        <ResponseBlock />
+        <Suspense
+          fallback={
+            <section className="flex h-screen w-full items-start justify-center border-t-8 border-gray bg-dark-blue px-8 pt-16 transition-all sm:mt-[120px] sm:h-[calc(100vh-120px)] sm:min-h-0 sm:w-1/2 sm:border-t-0 sm:px-8 sm:pt-0">
+              <Spinner />
+            </section>
+          }
+        >
+          <ResponseBlock />
+        </Suspense>
         <Suspense fallback={<Spinner />}>
           <SchemaBlock />
         </Suspense>
