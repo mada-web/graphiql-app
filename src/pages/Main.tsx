b70e078a04517752aaf96f63af18a3aad78ffad7
Mary190183 <pma190183@yandex.ru>
2023-05-19 16:12:36 +0300

    feat: add second header in main

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,13 @@
 import { FC, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 import ControlButtons from '../components/main/ControlButtons';
 import EditorBlock from '../components/main/EditorBlock';
 import QueryBlock from '../components/main/QueryBlock';
 import ResponseBlock from '../components/main/ResponseBlock';
 import SchemaBlock from '../components/main/SchemaBlock';
-import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebase';
 
 const Main: FC = (): JSX.Element => {
@@ -19,7 +20,10 @@ const Main: FC = (): JSX.Element => {
   }, [user, navigate, loading]);
 
   return (
-    <main className="relative z-0 min-h-screen w-screen overflow-hidden bg-dark-blue">
+    <main className="relative z-0 flex min-h-screen w-screen flex-col overflow-hidden bg-dark-blue">
+      <div className="mt-[80px] flex h-[40px] w-screen items-center justify-end bg-green p-2 text-white">
+        <p className="mx-auto max-w-7xl">Hello, {user?.email}!</p>
+      </div>
       <div className="relative flex flex-col sm:flex-row">
         <div className="relative grid h-screen grid-cols-[80%_20%] grid-rows-[auto_auto] pt-[120px] transition-all sm:w-1/2 sm:grid-cols-[85%_15%] md:grid-cols-[90%_10%]">
           <EditorBlock />
