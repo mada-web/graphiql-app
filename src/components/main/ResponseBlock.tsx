6e05584697902e487ba8acbe02c741abfe027c5c
dairinka <interest4me@gmail.com>
2023-05-21 20:05:25 +0300

    fix: correct height of blocks in main page

--- src/components/main/ResponseBlock.tsx
+++ src/components/main/ResponseBlock.tsx
@@ -3,7 +3,7 @@ import useAppContext from '../../hooks/useAppContext';
 const ResponseBlock = () => {
   const { responseApi } = useAppContext();
   return (
-    <section className="response h-screen w-full grow border-t-8 border-gray bg-dark-blue px-8 pt-16 text-sm text-gray transition-all sm:mt-[20px] sm:h-[calc(100vh-120px)] sm:min-h-0 sm:w-auto sm:min-w-[25%] sm:max-w-full sm:border-t-0 sm:pl-8 sm:pr-4 sm:pt-0">
+    <section className="response h-screen w-full grow border-t-8 border-gray bg-dark-blue px-8 pt-10 text-sm text-gray transition-all sm:mt-[20px] sm:h-[calc(100vh-140px)] sm:min-h-0 sm:w-auto sm:min-w-[25%] sm:max-w-full sm:border-t-0 sm:pl-8 sm:pr-4 sm:pt-0">
       <pre className="scrollbar h-[calc(100%-2rem)] w-full overflow-scroll pr-3 text-green transition-all sm:scroll-ml-3">
         {responseApi}
       </pre>
