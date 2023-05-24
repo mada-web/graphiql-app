41d6d0ba4f34090bbaad9f0ba31de3b79bf514fb
Mary190183 <pma190183@yandex.ru>
2023-05-24 21:45:49 +0300

    fix: button in home is Link

--- src/components/Home/InfoPokeApi.tsx
+++ src/components/Home/InfoPokeApi.tsx
@@ -1,4 +1,5 @@
 import { FormattedMessage, useIntl } from 'react-intl';
+import { Link } from 'react-router-dom';
 
 const InfoPokeApi = () => {
   const intl = useIntl();
@@ -21,13 +22,13 @@ const InfoPokeApi = () => {
         <p>
           <FormattedMessage id="INTRO_API5" />
         </p>
-        <form className="mt-4 w-[215px]" action="/sign-up">
+        <Link className="mt-4 w-[215px]" to="sign-up">
           <input
-            className="w-full rounded-md bg-green p-3 transition-all hover:bg-green/70 active:bg-orange"
+            className="w-full rounded-md bg-green p-3 text-white transition-all hover:bg-green/70 active:bg-orange"
             type="submit"
             value={intl.formatMessage({ id: 'SIGN_UP' })}
           />
-        </form>
+        </Link>
       </div>
     </>
   );
